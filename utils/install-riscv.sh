b8493b0bcbac6cfa03ac0fffb4430858370887f3
Enna1 <xumingjie1995@outlook.com>
2023-02-03 11:19:54 +0800

    test

--- utils/install-riscv.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-# This script is being called from .github/workflows/maketest.yml to install
-# necessary software to build/run tests in CI.
-
-sudo apt-get update
-sudo apt-get install -y gcc-riscv64-linux-gnu gcc-riscv64-unknown-elf
-sudo apt-get install -y qemu-system-riscv64 qemu-user
